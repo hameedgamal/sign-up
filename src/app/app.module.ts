598760ba4f62b4d0d6ae51de9366a5f99f125d86
Hamid Eltantawi <abdelhamid@backbase.com>
2021-03-25 21:29:42 +0100

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpFormComponent
   ],
   imports: [
     BrowserModule
