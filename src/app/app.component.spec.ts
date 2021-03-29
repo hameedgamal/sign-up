0b35bf5374d105f0a0cba5085ae9807829934d9d
Hamid Eltantawi <hameed.osman89@gmail.com>
2021-03-29 03:01:18 +0200

    Fix failing unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Sign up');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('sign-up app is running!');
-  });
 });
--- src/app/components/sign-up-form/sign-up-form.component.spec.ts
+++ src/app/components/sign-up-form/sign-up-form.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignUpFormComponent } from './sign-up-form.component';
@@ -8,7 +9,8 @@ describe('SignUpFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignUpFormComponent ]
+      declarations: [ SignUpFormComponent ],
+      imports: [HttpClientModule]
     })
     .compileComponents();
   });
